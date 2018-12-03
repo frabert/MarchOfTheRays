b7fa94496d8f88d7c7fea97d9bb2014fd4a6df8d
Francesco Bertolaccini <bertolaccinifrancesco@gmail.com>
2018-12-03 15:04:37 +0100

    Render shadows

--- MarchOfTheRays.CpuRenderer/Renderer.cs
+++ MarchOfTheRays.CpuRenderer/Renderer.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.Linq.Expressions;
 using System.Numerics;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,11 +12,54 @@ namespace MarchOfTheRays.CpuRenderer
         Vector3 cameraOrigin = new Vector3(2.1f, 2.0f, 2.5f);
         Vector3 cameraTarget = Vector3.Zero;
         Vector3 upDirection = Vector3.UnitY;
+        Vector3 lightDirection = Vector3.Normalize(new Vector3(0.3f, 1, 0));
+
+        float ambient_f = 0.1f;
+
+        Vector3 ambient_color = new Vector3(0.9f, 0.9f, 1.0f);
+
+        float diffuse_f = 0.4f;
+        float specular_f = 0.1f;
 
         int MAX_ITER = 100; // 100 is a safe number to use, it won't produce too many artifacts and still be quite fast
+        int MAX_SHADOW_ITER = 200;
         float MAX_DIST = 20.0f; // Make sure you change this if you have objects farther than 20 units away from the camera
         float EPSILON = 0.001f; // At this distance we are close enough to the object that we have essentially hit it
         float STEP_SIZE = 1.0f;
+
+        float softShadow(Vector3 ro, Vector3 rd, float start, float end, float k, Func<Vector3, float> distFunc)
+        {
+            float shade = 1.0f;
+
+            // The "start" value, or minimum, should be set to something more than the stop-threshold, so as to avoid a collision with 
+            // the surface the ray is setting out from. It doesn't matter how many times I write shadow code, I always seem to forget this.
+            // If adding shadows seems to make everything look dark, that tends to be the problem.
+            float dist = start;
+            float stepDist = end / MAX_SHADOW_ITER;
+
+            // Max shadow iterations - More iterations make nicer shadows, but slow things down. Obviously, the lowest 
+            // number to give a decent shadow is the best one to choose. 
+            for (int i = 0; i < MAX_SHADOW_ITER; i++)
+            {
+                // End, or maximum, should be set to the distance from the light to surface point. If you go beyond that
+                // you may hit a surface not between the surface and the light.
+                float h = distFunc(ro + rd * dist);
+                shade = Math.Min(shade, k * h / dist);
+
+                // What h combination you add to the distance depends on speed, accuracy, etc. To be honest, I find it impossible to find 
+                // the perfect balance. Faster GPUs give you more options, because more shadow iterations always produce better results.
+                // Anyway, here's some posibilities. Which one you use, depends on the situation:
+                // +=h, +=clamp( h, 0.01, 0.25 ), +=min( h, 0.1 ), +=stepDist, +=min(h, stepDist*2.), etc.
+                dist += Math.Min(h * STEP_SIZE, stepDist * 2.0f); // The best of both worlds... I think. 
+
+                // Early exits from accumulative distance function calls tend to be a good thing.
+                if (h < 0.001f || dist > end) break;
+            }
+
+            // I've added 0.3 to the final shade value, which lightens the shadow a bit. It's a preference thing. Really dark shadows look 
+            // too brutal to me.
+            return Math.Min(Math.Max(shade, 0) + 0.3f, 1.0f);
+        }
 
         public Renderer(Vector3 cameraOrigin, Vector3 cameraTarget, Vector3 upDirection, int maxIter, float maxDist, float Epsilon, float stepSize)
         {
@@ -30,6 +71,30 @@ namespace MarchOfTheRays.CpuRenderer
             MAX_DIST = maxDist;
             EPSILON = Epsilon;
             STEP_SIZE = stepSize;
+        }
+
+        static float clamp(float a, float min, float max)
+        {
+            return a < min ? min : a > max ? max : a;
+        }
+
+        float calculateAmbientOcclusion(Vector3 pos, Vector3 norm, Func<Vector3, float> distFunc)
+        {
+            const int AO_SAMPLES = 6;
+            float r = 0.0f;
+            float w = 1.0f;
+            for (int i = 1; i < AO_SAMPLES; i++)
+            {
+                float d0 = i * 0.2f;
+                r += w * (d0 - distFunc(pos + norm * d0));
+                w *= 0.5f;
+            }
+            return 1.0f - clamp(r, 0.0f, 1.0f);
+        }
+
+        Vector3 reflect(Vector3 I, Vector3 N)
+        {
+            return I - 2.0f * Vector3.Dot(N, I) * N;
         }
 
         void MainRender(float x, float y, float width, float height, out Vector3 outColor, Func<Vector3, float> distFunc)
@@ -72,9 +137,15 @@ namespace MarchOfTheRays.CpuRenderer
                     distFunc(pos + xyx) - distFunc(pos - xyx),
                     distFunc(pos + xxy) - distFunc(pos - xxy)));
 
-                float diffuse = Math.Max(0.0f, Vector3.Dot(-rayDir, normal));
-                float specular = (float)Math.Pow(diffuse, 64.0f);
-                outColor = new Vector3(diffuse + specular);
+                
+                float diffuse = Math.Max(0.0f, Vector3.Dot(lightDirection, normal));
+
+                var reflectDir = reflect(-lightDirection, rayDir);
+
+                float specular = (float)Math.Pow(Math.Max(0.0f, Vector3.Dot(rayDir, reflectDir)), 32.0f) * specular_f;
+                float ao = calculateAmbientOcclusion(pos, normal, distFunc);
+                float shade = softShadow(pos, lightDirection, EPSILON * 2, MAX_DIST, 8, distFunc);
+                outColor = (new Vector3(diffuse * shade + specular) + ambient_color * ambient_f) * ao;
             }
             else
             {
@@ -99,9 +170,9 @@ namespace MarchOfTheRays.CpuRenderer
 
                     Vector3 color;
                     MainRender(x, y + yoff, totalWidth, totalHeight, out color, func);
-                    rawBytes[idx + 0] = (byte)(Math.Min(color.X, 1.0f) * 255);
+                    rawBytes[idx + 0] = (byte)(Math.Min(color.Z, 1.0f) * 255);
                     rawBytes[idx + 1] = (byte)(Math.Min(color.Y, 1.0f) * 255);
-                    rawBytes[idx + 2] = (byte)(Math.Min(color.Z, 1.0f) * 255);
+                    rawBytes[idx + 2] = (byte)(Math.Min(color.X, 1.0f) * 255);
                     if(i % 100 == 99) progress?.Report(100);
                 }
             }
